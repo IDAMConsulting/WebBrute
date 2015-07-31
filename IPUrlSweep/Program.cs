b68d8cc14d9c369d58253dfb5ba6815c45d7ed02
Nathan Cocker <nrcocker@idamconsulting.co.uk>
2015-07-31 11:42:22 +0100

    Initial Commit

---
+++ IPUrlSweep/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LukeSkywalker.IPNetwork;
+using System.Diagnostics;
+using System.Net;
+using NLog;
+
+namespace IPUrlSweep
+{
+    class Program
+    {
+        static List<IPAddress> m_sHits = new List<IPAddress>();
+        static Logger _log = LogManager.GetCurrentClassLogger();
+
+        static bool DNSMatch(IPAddress addr, string Domain)
+        {
+            try
+            {
+                IPHostEntry dnsName = Dns.GetHostEntry(addr);
+
+                if (dnsName.HostName == Domain)
+                {
+                    _log.Debug("Match! {0} -> {1}. Target {2}.", addr, dnsName.HostName, Domain);
+                    return true;
+                }
+
+                _log.Debug("{0} -> {1}. Target {2}.", addr, dnsName.HostName, Domain);
+            }
+            catch (Exception exp) 
+            {
+                _log.Error(exp);
+            }
+
+            return false;
+        }
+
+        static int GetResource(string Url)
+        {
+            int code = 0;
+
+            try
+            {
+                WebClient cli = new WebClient();
+
+                string Content = cli.DownloadString(new Uri(Url));
+
+                code = 200;
+
+                _log.Debug("Url: {0} -> Response: {1} -> Content: {2}", Url, code, Content);
+            }
+            catch (WebException exp)
+            {
+                _log.Error(exp);
+                if (exp.Response != null)
+                {
+                    HttpWebResponse response = (HttpWebResponse)exp.Response;
+                    code = Convert.ToInt32(response.StatusCode);
+                    _log.Debug("{0} returned status code {1}", Url, code);
+                }
+            }
+
+            return code;
+        }
+
+        static void ProcessRange(string Range, string Pattern, string Code, string targetDNS)
+        {
+            IPNetwork network = IPNetwork.Parse(Range);
+            IPAddressCollection addresses = IPNetwork.ListIPAddress(network);
+            int expectedCode = Int32.Parse(Code);
+            foreach (IPAddress address in addresses)
+            {
+                Console.CursorLeft = 0;
+                Console.CursorTop = 0;
+                _log.Debug("Address: {0}", address.ToString());
+                Console.Write("Address:     {0}         ", address.ToString());
+
+                bool targetMatch = false;
+
+                if (targetDNS != string.Empty)
+                {
+                    targetMatch = DNSMatch(address, targetDNS);
+                }
+                else
+                {
+                    targetMatch = true;
+                }
+
+                if (targetMatch)
+                {
+                    
+                    string url = "http://" + address.ToString() + "/" + Pattern;
+
+                    _log.Debug("DNS Match.... created url {0}", url);
+
+                    int resp = GetResource(url);
+
+                    if (resp == expectedCode)
+                    {
+                        m_sHits.Add(address);
+                        Console.CursorLeft = 0;
+                        Console.CursorTop = 1;
+                        _log.Debug("Address: {0} Code: {1} Expected: {2}", address.ToString(), resp, expectedCode);
+                        Console.Write("Address:     {0}         Code:           {1}     Expected:       {2}     **Confirmed**", address.ToString(), resp, expectedCode);
+                    }
+                    else
+                    {
+                        _log.Debug("Address: {0} Wanted: {1} Responded with: {2}", address, expectedCode, resp);
+                    }
+                }
+            }
+        }
+
+        // Usage IPUrlSweep IPRange,..,.. URLPattern HTTPStatusCode
+        static void Main(string[] args)
+        {
+            Stopwatch watch = new Stopwatch();
+            if (args.Length < 3)
+            {
+                _log.Error("Invalid command line.....");
+                Console.WriteLine("Usage: IPUrlSweep IPRange,...,... URLPattern HTTPStatusCode");
+                return;
+            }
+
+            string target = args[0];
+            string pattern = args[1];
+            string code = args[2];
+            string targetDNS = string.Empty;
+            if (args.Length == 4)
+            {
+                
+                targetDNS = args[3];
+            }
+
+            _log.Debug("Address Range: {0}", target);
+            _log.Debug("URL Pattern: {0}", pattern);
+            _log.Debug("Required Response Code: {0}", code);
+            _log.Debug("Target DNS: {0}", targetDNS);
+
+            watch.Start();
+            // check the IPRange portions
+            // is it an array of ranges
+            if (target.Contains(','))
+            {
+                string[] ranges = target.Split(',');
+
+                foreach (string range in ranges)
+                {
+                    ProcessRange(range, pattern, code, targetDNS);
+                }
+            }
+            else
+            {
+                // Single range
+                ProcessRange(target, pattern, code, targetDNS);
+            }
+
+            watch.Stop();
+            Console.Clear();
+            Console.WriteLine("Confirmed Servers IPs.......");
+            foreach (IPAddress addr in m_sHits)
+            {
+                _log.Info("Address: {0} -> Hit!", addr);
+                Console.WriteLine("Address:   {0}   ", addr);
+            }
+            _log.Info("Completed in {0}", watch.Elapsed);
+            Console.WriteLine("Process Completed in {0}", watch.Elapsed);
+        }
+    }
+}
---
+++ WebBrute/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace WebBrute
+{
+    public class LoginAttempt
+    {
+        public string ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime AttemptTime { get; set; }
+        public int ResponseCode { get; set; }
+        public string RawResponse { get; set; }
+        public bool Success { get; set; }
+    }
+
+    public class Brutal
+    {
+        public List<LoginAttempt> Attempts { get; set; }
+        public List<string> Usernames { get; set; }
+        public List<string> Passwords { get; set; }
+        public string Target { get; set; }
+
+        public static Brutal Load(string JSON)
+        {
+            return JsonConvert.DeserializeObject<Brutal>(JSON);
+        }
+
+        public string Save()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+        public void LoadUsers(string Filename)
+        {
+            if (Usernames == null)
+            {
+                Usernames = new List<string>();
+            }
+
+            if(File.Exists(Filename))
+            {
+                Usernames = Usernames.Concat(File.ReadAllLines(Filename).ToList()).ToList();
+            }
+        }
+
+        public void LoadPasswords(string Filename)
+        {
+            if (Passwords == null)
+            {
+                Passwords = new List<string>();
+            }
+
+            if (File.Exists(Filename))
+            {
+                Passwords = Passwords.Concat(File.ReadAllLines(Filename).ToList()).ToList();
+            }
+        }
+
+        public void Process(string strTarget)
+        {
+            Target = strTarget;
+            string RespData = string.Empty;
+            int code = 0;
+            bool bSuccess = false;
+
+            Console.CursorLeft = 0;
+            Console.CursorTop = 0;
+            Console.Write("Processing URL: {0}", strTarget);
+
+            int ucount = 1;
+            int pcount = 1;
+
+            if (Attempts == null)
+            {
+                Attempts = new List<LoginAttempt>();
+            }
+
+            foreach (var user in Usernames)
+            {
+                Console.CursorLeft = 0;
+                Console.CursorTop = 1;
+                Console.Write("Username {0} of {1}", ucount++, Usernames.Count);
+                foreach (var pwd in Passwords)
+                {
+                    Console.CursorLeft = 0;
+                    Console.CursorTop = 2;
+                    Console.Write("Password {0} of {1}", pcount++, Passwords.Count);
+                    Console.CursorLeft = 0;
+                    Console.CursorTop = 3;
+                    Console.Write("Trying user: {0}     Password: {1}                   ", user, pwd);
+                    WebClient cli = new WebClient();
+                    string credentials = Convert.ToBase64String(
+                        Encoding.ASCII.GetBytes(user + ":" + pwd));
+                    cli.Headers[HttpRequestHeader.Authorization] = string.Format(
+                        "Basic {0}", credentials);
+                    cli.UseDefaultCredentials = false;
+
+                    try
+                    {
+                        RespData = cli.DownloadString(Target);
+                        Console.CursorLeft = 0;
+                        Console.CursorTop = 4;
+                        Console.Write("Success.....");
+                        bSuccess = true;
+                        break;
+                    }
+                    catch (WebException exp)
+                    {
+                        if (exp.Status == WebExceptionStatus.ProtocolError)
+                        {
+                            Console.CursorLeft = 0;
+                            Console.CursorTop = 4;
+                            Console.Write("Failed authentication....");
+                            StreamReader rdr = new StreamReader(exp.Response.GetResponseStream());
+                            RespData = rdr.ReadToEnd();
+                            rdr.Close();
+                        }
+
+                        HttpWebResponse response = (HttpWebResponse)exp.Response; 
+                        code = Convert.ToInt32(response.StatusCode);
+                    }
+                    catch (Exception e)
+                    {
+                        code = 0;
+                        Console.WriteLine(e);
+                    }
+
+                    // Build an attempt
+                    LoginAttempt attempt = new LoginAttempt();
+                    attempt.AttemptTime = DateTime.Now;
+                    attempt.ID = Guid.NewGuid().ToString();
+                    attempt.Password = pwd;
+                    attempt.RawResponse = RespData;
+                    attempt.ResponseCode = code;
+                    attempt.Username = user;
+
+                    if (code == 200)
+                    {
+                        attempt.Success = true;
+                    }
+                    else
+                    {
+                        attempt.Success = false;
+                    }
+
+                    Attempts.Add(attempt);
+
+                    // wait for 2 seconds between attempts
+                    Thread.Sleep(500);
+                }
+
+                if (bSuccess)
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Usernames = args[0];
+            string Passwords = args[1];
+
+            Brutal brute = new Brutal();
+            brute.LoadUsers(Usernames);
+            brute.LoadPasswords(Passwords);
+
+            brute.Process(args[2]);
+
+            File.WriteAllText(args[3], brute.Save());
+
+            Console.ReadKey();
+        }
+    }
+}
